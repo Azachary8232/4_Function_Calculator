84178ba00d7bbb7a5dade368cd4362eefb288a55
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-21 19:56:56 -0600

    Calculate Complete

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-
+/*
 var displayDiv = document.querySelector("#display");
 
 var countNum = 0;
 var countString = "";
+var subTotal = 0;
 var total;
 
 
@@ -17,8 +18,100 @@ function press(num){
 
 
 function setOP(operator){
-    total = countNum + operator;
-    console.log(total);
-    countString = 0;
+    subTotal = subTotal + countNum + operator;
+    console.log(subTotal);
+    countString ="";
+    console.log(countString);
     displayDiv.innerText = countString;
+
 }
+
+function calculate(){
+    console.log(subTotal);
+    console.log(countString);
+    total = parseFloat(subTotal),(*),parseFloat(countString);
+    console.log(total);
+}*/
+
+var displayDiv = document.querySelector("#display");
+
+var countNum = 0;
+var countString = "";
+var symbol;
+var countNum2;
+var total;
+
+function press(num){
+    countString += num;
+    console.log(countString);
+    displayDiv.innerText = countString;
+    countNum = parseFloat(countString);
+    console.log(countNum);
+}
+
+
+function setOP(operator){
+    if( operator === "+"){
+        symbol = "+";
+        console.log(symbol);
+    }
+
+    if( operator === "-"){
+        symbol = "-";
+        console.log(symbol);
+    }
+
+    if( operator === "/"){
+        symbol = "/";
+        console.log(symbol);
+    }
+
+    if( operator === "*"){
+        symbol = "*";
+        console.log(symbol);
+    }
+    countString = "";
+    countNum2 = countNum;
+    console.log(countNum2);
+}
+
+function calculate(){
+    if( symbol === "+"){
+        total = countNum2 + countNum;
+        console.log(total);
+        displayDiv.innerText = total;
+    }
+
+    if( symbol === "-"){
+        total = countNum2 - countNum;
+        console.log(total);
+        displayDiv.innerText = total;
+    }
+
+    if( symbol === "/"){
+        total = countNum2 / countNum;
+        console.log(total);
+        displayDiv.innerText = total;
+    }
+
+    if( symbol === "*"){
+        total = countNum2 * countNum;
+        console.log(total);
+        displayDiv.innerText = total;
+    }
+    
+    countNum = total;
+
+}
+
+
+
+
+/*
+function calculate(){
+    console.log(subTotal);
+    console.log(countString);
+    total = parseFloat(subTotal),(*),parseFloat(countString);
+    console.log(total);
+}*/
+
